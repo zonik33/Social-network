7ccd7a23d649fd7258269dbac80bfe1a30dfde5e
zonik33 <thezonikk@gmail.com>
2022-04-02 11:06:01 +0300

    ref gialogs

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -2,8 +2,33 @@ import s from './Dialogs.module.css';
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.alldialogs}>
+            <div className={s.dialogs_item}>
+                <div className={s.dialog}>
+                    <div className={s.img}>
+                        <img src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/ea7a3c32163929.567197ac70bda.png">
+                            </img>
+                    Roma
+                    </div>
+                </div>
+                <div className={s.dialog}>
+                <div className={s.img}>
+                        <img src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/ea7a3c32163929.567197ac70bda.png">
+                            </img>
+                    Vlad
+                    </div>
+                </div>
+            </div>
+            <div className={s.message}>
+                <div className={s.dialog}>
+                <div className={s.img}>
+                        <img src="https://cdn-icons-png.flaticon.com/512/84/84139.png">
+                            </img>HI! </div></div>
+                <div className={s.dialog} >
+                <div className={s.img}>
+                        <img src="https://cdn-icons-png.flaticon.com/512/84/84139.png">
+                            </img>Hello! </div></div>
+            </div>
         </div>
     )
 
