03ccea323a2e14fdcd677dd19762c35362f11bfd
zonik33 <thezonikk@gmail.com>
2022-04-02 10:17:38 +0300

    Проверка

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -5,19 +5,19 @@ const Nav = () => {
   return (
     <nav className={s.nav}>
       <div className={s.item}>
-        <NavLink to="/profile">Profile</NavLink>
+        <NavLink to="/profile" className = { navData => navData.isActive ? s.active : s.item }>Profile</NavLink>
       </div>
       <div className={s.item}>
-        <NavLink to="/dialogs">Messages</NavLink>
+        <NavLink to="/dialogs" className = { navData => navData.isActive ? s.active : s.item }>Messages</NavLink>
       </div>
       <div className={s.item}>
-        <NavLink to ="/news">News</NavLink>
+        <NavLink to ="/news" className = { navData => navData.isActive ? s.active : s.item }>News</NavLink>
       </div>
       <div className={s.item}>
-        <NavLink to ="/music">Music</NavLink>
+        <NavLink to ="/music" className = { navData => navData.isActive ? s.active : s.item }>Music</NavLink>
       </div>
       <div className={s.item}>
-        <NavLink to ="/settings">Settings</NavLink>
+        <NavLink to ="/settings" className = { navData => navData.isActive ? s.active : s.item }>Settings</NavLink>
       </div>
     </nav>
   );
